bb674f169543f8e68ca48c8556c32a0757e4066a
HoangMinh23062002 <minh.hoang23@student.passerellesnumeriques.org>
2022-07-27 21:25:06 +0700

    update st

--- src/components/pages/_BasePage.jsx
+++ src/components/pages/_BasePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Home from './Home';
 import Header from '../layouts/header';
-import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
 import StudentProfile from '../../components/pages/StudentProfile';
 import Joblistingjobs from '../pages/Joblistingjobs'
@@ -12,7 +11,7 @@ export default function _BasePage() {
   return(
       <div>
         <Header/>
-        <Account/>
+        <Joblistingjobs/>
         <Footer/>
       </div>
   )
