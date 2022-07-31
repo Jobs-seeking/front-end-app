2140bd6893bd9d95f76b9c0f5f623ebfb271a555
ThaooNguyenn <ronnguyen2862@gmail.com>
2022-07-31 18:12:36 +0700

    nn update form apply.

--- src/components/core-ui/formAppy.jsx
+++ src/components/core-ui/formAppy.jsx
@@ -1,32 +1,47 @@
+import Button from './Button';
+import { useState } from 'react';
+// import Input from './Input';
 
 const FormApply = () => {
+    const [info, setInfo] = useState([]);
+    const onChangeHandle = (key, value) => {
+        setInfo({...info, [key]: value})
+    }
+    const onSubmitHandle = (e) => {
+        e.preventDefault();
+        const data = new FormData();
+        data.append('name', info.name)
+        console.log(data);
+    }
     return(
         <>
-            {/* <h1>CV APPLICATION FORM</h1> */}
             <div className="core-ui__formApply">
-                <form action="" className="form">
+                <form onSubmit={onSubmitHandle} action="" className="form">
                     <div className="row">
                         <div className="col-left">
                             <label> Name</label>
-                            <input type="text" className="form-input" placeholder="Enter your first name" />
+                            <input type="text" className="form-input" placeholder="Enter your first name" onChange = {(e) => onChangeHandle('name', e.target.value)} />
                         </div>
                         <div className="col-right">
                             <label>Phone</label>
-                            <input type="text" className="form-input" placeholder="Enter your last name" />
+                            <input type="text" className="form-input" placeholder="Enter your last name" onChange = {(e) => onChangeHandle('phone', e.target.value)} />
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="col-left">
                             <label>Your email</label>
-                            <input type="email" id="email" name="email" placeholder="Enter your email"/>
+                            <input type="email" id="email" placeholder="Enter your email" onChange = {(e) => onChangeHandle('email', e.target.value)}/>
                         </div>
-                       
+                        <div className="col-right">
+                            <label>Your address</label>
+                            <input type="text" id="text" placeholder="Enter your address" onChange = {(e) => onChangeHandle('address', e.target.value)}/>
+                        </div>
                     </div>
                     <div className="row">
                         <div className="col-left">
                             <label >Position Applying for</label>
-                                <select>
+                                <select onChange = {(e) => onChangeHandle('position', e.target.value)}>
                                     <option value="0">---Choose---</option>
                                     <option value="1">Mobile</option>
                                     <option value="2">Testing</option>
@@ -38,7 +53,7 @@ const FormApply = () => {
                         </div>
                         <div className="col-right">
                             <label >Year Experience</label>
-                                <select>
+                                <select onChange = {(e) => onChangeHandle('year_experience', e.target.value)}>
                                     <option value="0">---Year Experience---</option>
                                     <option value="1">1</option>
                                     <option value="2">2</option>
@@ -49,14 +64,15 @@ const FormApply = () => {
                     </div>
                     <div className="row">
                         <label >Upload Your Cover Letter</label>
-                        <input type="file" id="" placeholder="Ex:eamail@gmail.com"/>
+                        <input type="file" id="" placeholder="Ex:eamail@gmail.com" onChange = {(e) => onChangeHandle('cover_letter', e.target.files[0].name)}/>
                     </div>
                     <div className="row">
                         <label >Upload your CV</label>
-                        <input type="file" id="upload-photo" placeholder="Ex:eamail@gmail.com"/>
+                        <input type="file" id="upload-photo" placeholder="Ex:eamail@gmail.com" onChange = {(e) => onChangeHandle('cv', e.target.files[0].name)}/>
                     </div>
                     <div>
-                        <button className="btn">Submit</button>
+                        {/* <button className="btn">Submit</button> */}
+                        <Button>Submit</Button>
                     </div>
                 </form>
             </div>
