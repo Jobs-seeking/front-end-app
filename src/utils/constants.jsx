5161877a7e4357d548c8004e6149ebc9e35b6af9
vinhuyen <vinhuyen.theonly@gmail.com>
2022-08-03 20:55:10 +0700

    Update account.

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://127.0.0.1:8000/";
-export const API_URL = BASE_URL + "api/"
+export const BASE_URL = "https://62e5520fde23e263791ec4a2.mockapi.io/";
+export const API_URL = BASE_URL + ":user"
