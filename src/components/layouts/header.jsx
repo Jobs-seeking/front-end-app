db50643207d46e286dd680d1701de7fd2ea97fc9
hqtriaidenboizz <hoquoctri10b7@gmail.com>
2022-08-03 21:26:24 +0700

    update header 1

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import Button from "../core-ui/Button";
-import { Link } from "react-router-dom";
 import { ReactComponent as LogoPNV } from '../../assets/images/LogoPNV.svg';
 export default function Header() {
-    const OpentNavMobile = () => {
+    const OpentNavMobile = () =>{
         var x = document.getElementById("nav-list");
-        if (x.className === "nav-list") {
+        if (x.className ==="nav-list"){
             x.className += "-navmobile";
-        } else {
-            x.className = "nav-list";
+        }else{
+            x.className ="nav-list";
         }
     }
     
-    const DCM =(evt) =>{
-        var i;
-        var x =document.getElementsByClassName("dcm");
-        for(i=0; i<x.length;i++){
-            x[i].className=x[i].className.replace(" active","")
-        }
-        evt.currentTarget.className += " active";
-    }
     return (
         <section className="navigation">
             <div className="nav-container">
@@ -29,23 +20,23 @@ export default function Header() {
                 <nav>
                     <ul className="nav-list" id="nav-list">
                         <li>
-                            <a  className="dcm" onClick={DCM} href="#!">RECRULTERS</a>
+                            <a href="#!">RECRULTERS</a>
                         </li>
                         <li>
-                            <Link  className="dcm"  onClick={DCM} to="/">HOME</Link>
+                            <a href="#!">HOME</a>
                         </li>
                         <li>
-                            <Link className="dcm" onClick={DCM} to="/jobs">JOBS</Link>
+                            <a href="#!">JOBS</a>
                         </li>
                         <li>
-                            <a className="dcm" onClick={DCM} href="#!">APPLY CV</a>
+                            <a href="#!">APPLY CV</a>
                         </li>
                     </ul>
                     <div onClick={OpentNavMobile} className="nav-mobile" id="nav-mobile"><span></span><span></span><span></span></div>
                     <div className="nav-button">
-                        <Link to="/login"><Button buttonStyle="btn--outline">LOGIN</Button></Link>
-                        <Button >SIGN UP</Button>
-                    </div>
+                    <Button buttonStyle="btn--outline">LOGIN</Button>
+                    <Button >SIGN UP</Button>
+                </div>
                 </nav>
             </div>
         </section>
