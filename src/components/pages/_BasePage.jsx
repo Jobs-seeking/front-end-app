24ed81a2c9c5f0d23340c11243879b1a2d112ef4
vinhuyen <vinhuyen.theonly@gmail.com>
2022-08-04 08:22:31 +0700

    Fix bug can not find module on _BasePage.

--- src/components/pages/_BasePage.jsx
+++ src/components/pages/_BasePage.jsx
@@ -4,7 +4,7 @@ import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
 import StudentProfile from '../../components/pages/StudentProfile';
 import Joblistingjobs from '../pages/Joblistingjobs'
-import FormApply from '../core-ui/formAppy'
+import FormApply from '../core-ui/FormApply'
 import EditStudentAccount from '../pages/EditStudentAccount'
 import Account from '../pages/Account'
 import {Outlet} from 'react-router-dom';
