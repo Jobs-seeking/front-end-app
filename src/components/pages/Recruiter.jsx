65bc4a28ca448b9bb0a2e0f016e0ca4f31e17fa2
vinhuyen <vinhuyen.theonly@gmail.com>
2022-08-09 23:39:20 +0700

    Update recruiter.

--- src/components/pages/Recruiter.jsx
+++ src/components/pages/Recruiter.jsx
@@ -13,10 +13,12 @@ export default function Recruiter() {
         </div>
 
         <form className="recruiter__form" action="">
+
           <div className="input-field">
             <label htmlFor="job-title"></label>
             <Input type="text" name="job-title" placeholder="Job title 1"/>
           </div>
+
           <select className='input-field'>
               <option value="0">---Choose---</option>
               <option value="1">Mobile</option>
@@ -26,18 +28,22 @@ export default function Recruiter() {
               <option value="5">Full Stack </option>
               <option value="6">BA</option>
           </select>
+
           <div className="input-field" id={"input-field-3"}>
             <label htmlFor=""></label>
             <textarea name="job-title" placeholder="hahahah"/>
           </div>
+
           <div className="input-field">
             <label htmlFor="job-title"></label>
             <Input type="text" name="job-title" placeholder="JOB TITLE"/>
           </div>
+
           <div className="input-field">
             <label htmlFor="job-title"></label>
             <Input type="text" name="job-title" placeholder="JOB TITLE"/>
           </div>
+
           <select className='input-field'>
             <option value="0">---Choose---</option>
             <option value="1">Mobile</option>
@@ -47,6 +53,7 @@ export default function Recruiter() {
             <option value="5">Full Stack </option>
             <option value="6">BA</option>
           </select>
+
           <select className='input-field'>
             <option value="0">---Choose---</option>
             <option value="1">Mobile</option>
